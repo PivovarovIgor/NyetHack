6cf8263d81a2fafe945b5602f78a994ee8442258
PivIgor <brauer.igor@gmail.com>
2021-07-15 00:22:08 +0300

    Learning functions of the Kotlin programming language.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -4,7 +4,44 @@ fun main() {
     val isBlessed = true
     val isImmortal = false
 
-    val healthStatus = when (healthPoints) {
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+    ////named arguments
+//    printPlayerStatus(
+//        isBlessed = false,
+//        name = "NONE",
+//        auraColor = "black",
+//        healthStatus = "some health status"
+//    )
+
+    castFireball()
+}
+
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println("Aura: $auraColor (Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(
+    isBlessed: Boolean,
+    healthPoints: Int,
+    isImmortal: Boolean
+): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String =
+    when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -15,9 +52,7 @@ fun main() {
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful conditional!"
     }
-    println("$name $healthStatus")
 
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-    println("Aura: $auraColor (Blessed: ${if (isBlessed) "YES" else "NO"})")
-}
+
+private fun castFireball(numFireballs: Int = 2) =
+    println("A glass of Fireball springs into existence. (x$numFireballs)")
