1e1bffef37fa2310a45787c7eb35315bfbba2550
PivIgor <brauer.igor@gmail.com>
2021-07-16 23:25:17 +0300

    null safety and exceptions

---
+++ src/main/kotlin/Tavern.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var beverage = readLine()
+//    beverage = null
+    if (beverage != null) {
+        beverage = beverage.capitalize()
+    } else {
+        println("I can't do that without crashing - beverage was null!")
+    }
+    val beverageServed = beverage ?: "Buttered Ale"
+    println(beverageServed)
+}
